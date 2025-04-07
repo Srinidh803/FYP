ed3d192f443be406f88a2f2951cee99577b1e94b
Srinidh803 <chsrinidhrao@gmail.com>
2025-04-07 23:33:11 +0530

    Update settings.py

--- fyp/settings.py
+++ fyp/settings.py
@@ -24,7 +24,8 @@ BASE_DIR = Path(__file__).resolve().parent.parent
 # SECURITY WARNING: keep the secret key used in production secret!
 SECRET_KEY = os.environ.get('SECRET_KEY', 'fallback-secret-for-dev')
 DEBUG = os.environ.get('DEBUG', 'False') == 'True'
-ALLOWED_HOSTS = [os.getenv("ALLOWED_HOSTS", "localhost").split(",")]
+ALLOWED_HOSTS = os.getenv("ALLOWED_HOSTS", "localhost").split(",")
+
 
 
 
